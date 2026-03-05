6213122059ccfd9060ca9f73921852bdbc0a302f
Lezeto <nicolas0vf@gmail.com>
2026-03-05 12:28:05 -0300

    Euler 2

--- euler2.js
+++ euler2.js
@@ -1,14 +1,10 @@
-function sumEvenFibonacci(n) {
-  let a = 1;
-  let b = 2;
-  let sum = 0;
-  while (a <= n) {
-    if (a % 2 === 0) {
-      sum += a;
-    }
-    const next = a + b;
-    a = b;
-    b = next;
-  }
-  return sum;
+function fiboEvenSum(n) {
+  let a= 1;
+  let b= 2;
+  let suma =0;
+   while(b<=n){
+    if(b%2==0){suma+=b}
+    [a,b]=[b,a+b];
+   }
+   return suma;
 }
